0a4b0ef218b720e897e1f7a778ea6390d688167b
Arjun <khadearjun@gmail.com>
2026-01-25 20:47:23 +0530

    updated profile and usercard component

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { removeUserFromFeed } from '../store/feedSlice';
 
 const UserCard = ({user}) => {
+
+  const dispatch = useDispatch();
+
+   const handleAcceptRequest = () => {
+    dispatch(removeUserFromFeed(id))
+   }
+
 
     const {firstname, lastname, id, skills, about, photourl } = user;
     if(user)
     console.log(user)
-  return (
+  return user && (
     <div>
-        <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card bg-base-200 w-96 shadow-sm">
   <figure>
     <img
       src={user && photourl}
@@ -19,8 +28,8 @@ const UserCard = ({user}) => {
     <p>{skills.join(",")}</p>
 
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Ignore</button>
-      <button className="btn btn-secondary">Accept Invitation</button>
+      <button className="btn btn-primary" onClick={handleAcceptRequest}>Ignore</button>
+      <button className="btn btn-secondary" onClick={handleAcceptRequest} >Accept Invitation</button>
 
     </div>
   </div>
